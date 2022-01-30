ba27d2697d34f17e32ab6d3df6eb7098614c5b1c
manalipatil <mmpatil34@gmail.com>
2022-01-29 23:42:39 -0800

    auth on behalf of user

--- app/app/app.py
+++ app/app/app.py
@@ -4,19 +4,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-credentials = ('ee711872-3b83-46a4-a681-aff01ff4bdab','Utq7Q~i5IHQCyONc2uHIg-ZhNZSAlZ~k54rI9')
-token_backend = FileSystemTokenBackend(token_path='my_folder', token_filename='my_token.txt')
 
-account = Account(credentials, token_backend=token_backend)
-@app.get("/")
-def read_root():
-    if account.authenticate(scopes=['mailbox','mailbox_shared','address_book','address_book']):
+@app.get("/authenticate")
+def read_root(client_id: str, client_secert: str):
+    # credentials = ('ee711872-3b83-46a4-a681-aff01ff4bdab', 'Utq7Q~i5IHQCyONc2uHIg-ZhNZSAlZ~k54rI9')
+    credentials = (client_id, client_secert)
+    token_backend = FileSystemTokenBackend(token_path='my_folder', token_filename='my_token.txt')
+    account = Account(credentials, token_backend=token_backend)
+    if account.authenticate(scopes=['mailbox', 'mailbox_shared', 'address_book', 'address_book']):
         return {"Authenticated !"}
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/mails/read")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-

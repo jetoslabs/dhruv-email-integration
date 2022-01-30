8bd57a9ee49285abf0f5eb062d6b9e067e4ef715
manalipatil <mmpatil34@gmail.com>
2022-01-29 23:25:18 -0800

    auth on behalf of user

--- app/app/app.py
+++ app/app/app.py
@@ -1,12 +1,17 @@
 from typing import Optional
-
+from O365 import Account, FileSystemTokenBackend
 from fastapi import FastAPI
 
 app = FastAPI()
 
+credentials = ('ee711872-3b83-46a4-a681-aff01ff4bdab','Utq7Q~i5IHQCyONc2uHIg-ZhNZSAlZ~k54rI9')
+token_backend = FileSystemTokenBackend(token_path='my_folder', token_filename='my_token.txt')
 
+account = Account(credentials, token_backend=token_backend)
 @app.get("/")
 def read_root():
+    if account.authenticate(scopes=['mailbox','mailbox_shared','address_book','address_book']):
+        return {"Authenticated !"}
     return {"Hello": "World"}
 
 

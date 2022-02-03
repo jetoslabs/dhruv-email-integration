389857f47d5e2ab23093ff8d79eff6507f54f560
Anurag Jha <toanuragjha@gmail.com>
2022-02-02 22:09:29 -0800

    chore

--- app/app/api/v1/auth.py
+++ app/app/api/v1/auth.py
@@ -1,16 +1,4 @@
-# from O365 import FileSystemTokenBackend, Account
+
 from fastapi import APIRouter
 
 router = APIRouter()
-
-
-# @router.get("/")
-# def read_root(client_id: str, client_secert: str):
-#     # credentials = ('ee711872-3b83-46a4-a681-aff01ff4bdab', 'Utq7Q~i5IHQCyONc2uHIg-ZhNZSAlZ~k54rI9')
-#     credentials = (client_id, client_secert)
-#     token_backend = FileSystemTokenBackend(token_path='my_folder', token_filename='my_token.txt')
-#     account = Account(credentials, token_backend=token_backend)
-#     globalAccount = account
-#     if account.authenticate(scopes=['mailbox', 'mailbox_shared', 'address_book', 'address_book']):
-#         return {"Authenticated !"}
-#     return {"Hello": "World"}

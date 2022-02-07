04c0a422fe8ed10c1a78880017f662a2e658da50
Anurag Jha <toanuragjha@gmail.com>
2022-02-06 16:42:18 -0800

    mail apis init

--- app/app/api/v1/mail.py
+++ app/app/api/v1/mail.py
@@ -1,10 +1,14 @@
 import json
+import base64
+from io import BytesIO
 
 from fastapi import APIRouter
 
 from app.apiclients.api_client import ApiClient
+from app.apiclients.aws_client import AWSClientHelper, boto3_session
 from app.apiclients.endpoint_ms import MsEndpointsHelper, endpoints_ms, MsEndpointHelper
 from app.core.auth import get_auth_config_and_confidential_client_application_and_access_token
+from app.core.config import global_config
 
 router = APIRouter()
 
@@ -29,6 +33,77 @@ async def list_mail(tenant: str, id: str):
         print(token.get("error"))
         print(token.get("error_description"))
         print(token.get("correlation_id"))  # You may need this when reporting a bug
+
+
+@router.get("/{id}/messages/{message_id}/attachments")
+async def list_message_attachments(tenant: str, id: str, message_id: str):
+    config, client_app, token = get_auth_config_and_confidential_client_application_and_access_token(tenant)
+    if "access_token" in token:
+        endpoint = MsEndpointsHelper.get_endpoint("message:list:attachment", endpoints_ms)
+        endpoint.request_params["id"] = id
+        endpoint.request_params["message_id"] = message_id
+        url = MsEndpointHelper.form_url(endpoint)
+        api_client = ApiClient(endpoint.request_method, url, headers=ApiClient.get_headers(token), timeout_sec=3000)
+        response, data = await api_client.retryable_call()
+        return data
+    else:
+        print(token.get("error"))
+        print(token.get("error_description"))
+        print(token.get("correlation_id"))  # You may need this when reporting a bug
+
+
+@router.get("/{id}/messages/{message_id}")
+async def get_message(tenant: str, id: str, message_id: str):
+    config, client_app, token = get_auth_config_and_confidential_client_application_and_access_token(tenant)
+    if "access_token" in token:
+        endpoint = MsEndpointsHelper.get_endpoint("message:get", endpoints_ms)
+        endpoint.request_params["id"] = id
+        endpoint.request_params["message_id"] = message_id
+        url = MsEndpointHelper.form_url(endpoint)
+        api_client = ApiClient(endpoint.request_method, url, headers=ApiClient.get_headers(token), timeout_sec=3000)
+        response, data = await api_client.retryable_call()
+        return data
+    else:
+        print(token.get("error"))
+        print(token.get("error_description"))
+        print(token.get("correlation_id"))  # You may need this when reporting a bug
+
+@router.get("/{id}/messages/{message_id}/$value")
+async def get_message_mime(tenant: str, id: str, message_id: str):
+    config, client_app, token = get_auth_config_and_confidential_client_application_and_access_token(tenant)
+    if "access_token" in token:
+        endpoint = MsEndpointsHelper.get_endpoint("message:get:mime", endpoints_ms)
+        endpoint.request_params["id"] = id
+        endpoint.request_params["message_id"] = message_id
+        url = MsEndpointHelper.form_url(endpoint)
+        api_client = ApiClient(endpoint.request_method, url, headers=ApiClient.get_headers(token), timeout_sec=3000)
+        response, data = await api_client.retryable_call()
+        return data
+    else:
+        print(token.get("error"))
+        print(token.get("error_description"))
+        print(token.get("correlation_id"))  # You may need this when reporting a bug
+
+
+@router.get("/{id}/messages/{message_id}/attachments/save")
+async def save_message_attachment(tenant: str, id: str, message_id: str):
+    config, client_app, token = get_auth_config_and_confidential_client_application_and_access_token(tenant)
+    if "access_token" in token:
+        data = await list_message_attachments(tenant, id, message_id)
+        attachments: list = data["value"]
+        for attachment in attachments:
+            filename = attachment["name"]
+            content_b64 = attachment["contentBytes"]
+            # contentBytes is base64 encoded str
+            content = base64.b64decode(content_b64)
+            is_saved = await AWSClientHelper.save_to_s3(boto3_session, BytesIO(content), global_config.s3_root_bucket, filename)
+
+    else:
+        print(token.get("error"))
+        print(token.get("error_description"))
+        print(token.get("correlation_id"))  # You may need this when reporting a bug
+
+
 
 
 # @router.post("/sendMail")

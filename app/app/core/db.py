78d8b29501e3a97812b12e2aa1661ccd28220939
Anurag Jha <toanuragjha@gmail.com>
2022-02-07 00:57:49 -0800

    sqlalchemy session defined

---
+++ app/app/core/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import global_config
+
+# create an engine
+engine = create_engine(global_config.db_url, pool_pre_ping=True)
+
+# create a configured "Session" class
+Session = sessionmaker(bind=engine)
+
+# create a Session
+db_session = Session()

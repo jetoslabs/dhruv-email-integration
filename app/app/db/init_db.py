158dbbbedd8866c117f8e7a9359d0f6590f8ccb8
Anurag Jha <toanuragjha@gmail.com>
2022-02-10 02:29:06 -0800

    chore

--- app/app/db/init_db.py
+++ app/app/db/init_db.py
@@ -1,6 +1,3 @@
-
-from sqlalchemy.orm import Session
-
 from app.db import base
 
 # NOTE: All the tables to be created, first have to be imported in app/db/base.py
@@ -8,5 +5,3 @@ from app.db import base
 
 def init_db(engine) -> None:
     base.Base.metadata.create_all(engine)
-    # base.Base.cre.create_all(bind=db)
-    # base.CorrespondenceId.metadata.create(bind=db)

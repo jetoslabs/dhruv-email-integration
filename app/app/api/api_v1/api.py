3a408fa06fef71cf7efe922c9540e96e8e1fe578
Anurag Jha <toanuragjha@gmail.com>
2022-02-18 19:07:28 -0800

    user prefix for api moved to end files

--- app/app/api/api_v1/api.py
+++ app/app/api/api_v1/api.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 router.include_router(router=auth.router, prefix="/auth", tags=["auth"])
 
-router.include_router(router=users.router, prefix="/users", tags=["users"])
-router.include_router(router=mail.router, prefix="/users", tags=["mail"])
+router.include_router(router=users.router, prefix="", tags=["users"])
+router.include_router(router=mail.router, prefix="", tags=["mail"])
 
 router.include_router(router=test.router, prefix="/test", tags=["test"])

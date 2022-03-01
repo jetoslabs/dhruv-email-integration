efce01a7c7ef0b5e15e629feb7f0f7be79a4cd5e
Anurag Jha <toanuragjha@gmail.com>
2022-03-01 01:29:36 -0800

    msal client_app creating only once (correct)

--- app/app/core/auth.py
+++ app/app/core/auth.py
@@ -1,6 +1,9 @@
 #  MS Identity Platform
+import atexit
 import json
+import os
 import sys
+from functools import lru_cache
 from typing import Any, Union, Tuple, Dict, Optional
 
 import msal
@@ -30,20 +33,14 @@ def load_ms_auth_configs(filepath: str) -> MsAuthConfigs:
         configs = MsAuthConfigs(**configs_dict)
         return configs
     except Exception as e:
-        logger.bind(error=e, filepath=filepath).error(f"Error while loading ms_auth_configs, filepath = {filepath}... Exiting\n {e}")
+        logger.bind(error=e, filepath=filepath).error(
+            f"Error while loading ms_auth_configs, filepath = {filepath}... Exiting\n {e}")
         sys.exit(1)
 
 
 def get_ms_auth_config(tenant: str) -> MsAuthConfig:
     config = ms_auth_configs.configs[tenant]
     return config
-
-
-# def is_email_in_internal_domains(tenant: str, email: str) -> bool:
-#     tenant_auth_config = get_ms_auth_config(tenant)
-#     internal_domains: Optional[list] = tenant_auth_config.internal_domains
-#     if internal_domains is None: return False
-#     return email in internal_domains
 
 
 # TODO: move var - this is one of the many vars for global store
@@ -54,10 +51,23 @@ ms_auth_configs = load_ms_auth_configs(f"{settings.CONFIGURATION_PATH}configurat
 def get_confidential_client_application(config) -> ConfidentialClientApplication:
     # TODO: make ConfidentialClientApplication long-lived (should be 1 for each tenant)
     # Create a preferably long-lived app instance which maintains a token cache.
+    # cache = msal.SerializableTokenCache()
+    # if os.path.exists("my_cache.bin"):
+    #     cache.deserialize(open("my_cache.bin", "r").read())
+    # else:
+    #     # Creates a new file
+    #     with open("my_cache.bin", 'w') as fp:
+    #         pass
+    # atexit.register(lambda:
+    #                 open("my_cache.bin", "w").write(cache.serialize())
+    #                 # Hint: The following optional line persists only when state changed
+    #                 if cache.has_state_changed else None
+    #                 )
     app: ConfidentialClientApplication = msal.ConfidentialClientApplication(
         config.client_id,
         authority=config.authority,
         client_credential=config.secret,
+        # token_cache=cache
         # token_cache=...  # Default cache is in memory only.
         # You can learn how to use SerializableTokenCache from
         # https://msal-python.rtfd.io/en/latest/#msal.SerializableTokenCache
@@ -66,6 +76,7 @@ def get_confidential_client_application(config) -> ConfidentialClientApplication
 
 
 def get_access_token(config: MsAuthConfig, app: ConfidentialClientApplication):
+    logger.bind().info("Getting access token")
     # The pattern to acquire a token looks like this.
     result = None
 
@@ -81,11 +92,17 @@ def get_access_token(config: MsAuthConfig, app: ConfidentialClientApplication):
     return result
 
 
-def get_auth_config_and_confidential_client_application_and_access_token(tenant) -> Union[
-    Tuple[MsAuthConfig, ConfidentialClientApplication, Any], Tuple[None, None, None]]:
+client_app:  ConfidentialClientApplication = None
+
+
+def get_auth_config_and_confidential_client_application_and_access_token(
+        tenant
+) -> Union[Tuple[MsAuthConfig, ConfidentialClientApplication, Any], Tuple[None, None, None]]:
+    global client_app
     config = get_ms_auth_config(tenant)
     if config is not None:
-        client_app = get_confidential_client_application(config)
+        if client_app is None:
+            client_app = get_confidential_client_application(config)
         token = get_access_token(config, client_app)
         return config, client_app, token
     return config, None, None
